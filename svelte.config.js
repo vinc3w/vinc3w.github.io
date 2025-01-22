0f62a2297c59c63c7230c2e0cafe047df6e60431
vinc3w <vinc3w59@gmail.com>
2025-01-22 22:26:33 +0800

    rename fallback file

--- svelte.config.js
+++ svelte.config.js
@@ -7,7 +7,7 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
-			fallback: "404.html",
+			fallback: "error.html",
 		}),
 		paths: {
 			base: process.env.ENVIRONMENT === "prod" ? process.env.BASE : "",
