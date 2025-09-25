222a535fe73bdf21d8cd1787e6d3e0456d2f056d
vinc3w <vinc3w59@gmail.com>
2025-09-25 19:32:26 +0800

    Add animations

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,15 +2,34 @@
   import ButtonLink from "$lib/components/button-link/ButtonLink.svelte";
   import PageSetup from "$lib/components/page-setup/PageSetup.svelte";
   import FooterShadow from "$lib/components/footer/Shadow.svelte";
+  import gsap from "gsap";
 
   import { page } from "$app/state";
   import { HOME_ROUTE } from "$lib/data/routes";
+  import { onMount } from "svelte";
+  import { app } from "$lib/states/app.svelte";
+
+  const delay = (app.loadingTransitionMS / 2 + app.loadingDurationMS) / 1000;
+  let code;
+
+  onMount(() => {
+    gsap.from(code, {
+      autoAlpha: 0,
+      y: "100%",
+      opacity: 0,
+      duration: 1.2,
+      ease: "power4.out",
+      delay,
+    })
+  });
 </script>
 
 <PageSetup />
 
 <div class="error">
-  <div class="code">404</div>
+  <div class="code-container">
+    <div bind:this={code} class="code">404</div>
+  </div>
   <div class="description">The page you are looking for does not exist or has been moved.</div>
   <ButtonLink href={HOME_ROUTE}>Back</ButtonLink>
 </div>
@@ -31,9 +50,14 @@
     z-index: 100;
   }
 
+  .code-container {
+    overflow-y: hidden;
+  }
+
   .code {
     font-size: 384px;
     font-weight: 900;
     line-height: 100%;
+    visibility: hidden;
   }
 </style>
