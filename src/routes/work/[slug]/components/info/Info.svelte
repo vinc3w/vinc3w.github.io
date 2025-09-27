0ddb10eb19d75737fbb183ba1802a5f19697e88e
vinc3w <vinc3w59@gmail.com>
2025-09-27 11:22:41 +0800

    Fix description overflowing

--- src/routes/work/[slug]/components/info/Info.svelte
+++ src/routes/work/[slug]/components/info/Info.svelte
@@ -1,66 +1,64 @@
 <script>
-  import gsap from "gsap";
   import TextReveal from "$lib/components/text-reveal/TextReveal.svelte";
-
+  import gsap from "gsap";
   import { tags } from "$lib/data/works";
-  import { onMount } from "svelte";
 
   let { data } = $props();
-  let info;
-
-  onMount(() => {
-    gsap.to(info, {
-      y: 0,
-      opacity: 1,
-      duration: 1,
-      ease: "power4.out",
-      scrollTrigger: {
-        trigger: info,
-        start: "top bottom",
-      },
-    });
-  });
 </script>
 
-<div bind:this={info} class="info grid-layout">
-  <div class="description">
-    <TextReveal duration={0.6} stagger={0.02}>
-      {data.description}
-    </TextReveal>
+<div class="info">
+  <div class="row grid-layout">
+    <div class="description">
+      <TextReveal duration={0.6} stagger={0.02}>
+        {data.description}
+      </TextReveal>
+    </div>
   </div>
-  <div class="tags">
-    <div class="label">Tags</div>
-    <ul>
-      {#each data.tags as tag}
-        <li>{tags.find(t => t.value === tag).label}</li>
-      {/each}
-    </ul>
+  <div class="row grid-layout">
+    <div class="tools">
+      <div class="label">Tools</div>
+      <ul>
+        {#each data.tools as tool}
+          <li>{tool}</li>
+        {/each}
+      </ul>
+    </div>
+    <div class="tags">
+      <div class="label">Tags</div>
+      <ul>
+        {#each data.tags as tag}
+          <li>{tags.find(t => t.value === tag).label}</li>
+        {/each}
+      </ul>
+    </div>
+    <div class="link">
+      <div class="label">Link</div>
+      <a href={data.projectLink}>Here</a>
+    </div>
   </div>
-  <div class="link">
-    <div class="label">Link</div>
-    <a href={data.projectLink}>Here</a>
-  </div>
-  <div class="tools">
-    <div class="label">Tools</div>
-    <ul>
-      {#each data.tools as tool}
-        <li>{tool}</li>
-      {/each}
-    </ul>
+  <div class="row grid-layout">
+    <div class="tools">
+      <div class="label">Tools</div>
+      <ul>
+        {#each data.tools as tool}
+          <li>{tool}</li>
+        {/each}
+      </ul>
+    </div>
   </div>
 </div>
 
 <style>
   .info {
+    display: flex;
+    flex-direction: column;
+    align-items: stretch;
     row-gap: 128px;
     padding: 0 var(--x-padding);
-    transform: translateY(64px);
-    opacity: 0;
   }
 
   .description {
     grid-column: span 6;
-    grid-row: 1 / 2;
   }
 
   .label {
@@ -70,54 +68,69 @@
 
   .tags {
     grid-column: 7 / span 3;
-    grid-row: 2 / 3;
   }
 
   .link {
     grid-column: 10 / span 3;
-    grid-row: 2 / 3;
+  }
+
+  .row:nth-of-type(2) .tools {
+    display: none;
   }
 
   .tools {
     grid-column: 7 / span 3;
-    grid-row: 3 / 4;
   }
 
   @media (width <= 800px) {
+    .row:nth-of-type(1) {
+      display: block;
+    }
+    
     .description {
       grid-column: span 12;
     }
 
     .tags {
       grid-column: 5 / span 4;
-      grid-row: 2 / 3;
     }
 
     .link {
       grid-column: 9 / span 4;
-      grid-row: 2 / 3;
     }
 
     .tools {
       grid-column: span 4;
-      grid-row: 2 / 3;
+    }
+
+    .row:nth-of-type(2) .tools {
+      display: block;
+    }
+
+    .row:nth-of-type(3) .tools {
+      display: none;
     }
   }
 
   @media (width <= 550px) {
     .tags {
       grid-column: span 6;
-      grid-row: 2 / 3;
     }
 
     .link {
       grid-column: 7 / span 6;
-      grid-row: 2 / 3;
     }
 
     .tools {
       grid-column: span 12;
-      grid-row: 3 / 4;
+    }
+
+    .row:nth-of-type(2) .tools {
+      display: none;
+    }
+
+    .row:nth-of-type(3) .tools {
+      display: block;
     }
   }
 </style>
